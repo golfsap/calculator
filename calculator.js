76e24e192dacd2fe08b95bd66f565f6807c47243
Golfsap <golf.sap@gmail.com>
2023-08-21 14:11:55 +0700

    Add basic calculator operator functions

---
+++ calculator.js
@@ -0,0 +1,39 @@
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function operate(a,b,operator) {
+    switch(operator) {
+        case '+':
+            add(a,b);
+            break;
+        case '-':
+            subtract(a,b);
+            break;
+        case '*':
+            multiply(a,b);
+            break;
+        case '/':
+            divide(a,b);
+            break;
+        default:
+            console.log('No operator provided');
+    }
+} 
+
