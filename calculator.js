063f00f6869af6e07d4d058d9f1a792b540cbdad
Golfsap <golf.sap@gmail.com>
2023-08-24 09:35:18 +0700

    Add handleClick functions for numbers and operators

--- calculator.js
+++ calculator.js
@@ -1,6 +1,35 @@
-let num1 = 0;
-let num2 = 0;
-let operator = '';
+// UI elements
+const _0 = document.getElementById("zero");
+const _1 = document.getElementById("one");
+const _2 = document.getElementById("two");
+const _3 = document.getElementById("three");
+const _4 = document.getElementById("four");
+const _5 = document.getElementById("five");
+const _6 = document.getElementById("six");
+const _7 = document.getElementById("seven");
+const _8 = document.getElementById("eight");
+const _9 = document.getElementById("nine");
+const _add = document.getElementById("add");
+const _subtract = document.getElementById("subtract");
+const _multiply = document.getElementById("multiply");
+const _divide = document.getElementById("divide");
+
+const display = document.getElementById("display");
+const numbers = document.querySelectorAll(".num");
+const operators = document.querySelectorAll(".operator");
+
+// initialize variables 
+let num1 = '';
+let num2 = '';
+let op = '';
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => handleClickNumbers(number));
+});
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => handleClickOperators(operator));
+})
 
 function add(a,b) {
     return a+b;
@@ -37,3 +66,17 @@ function operate(a,b,operator) {
     }
 } 
 
+function handleClickNumbers(number) {
+    const numFromText = number.innerText;
+    num1+= numFromText;
+    display.innerText = num1;
+}
+
+function handleClickOperators(operator) {
+    op = operator.innerText;
+    // operator = operatorFromText;
+    console.log(op);
+    // display.innerText = num1;
+}
+
+console.log(numbers);
